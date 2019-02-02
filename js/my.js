66abf7118d59fbac598b1248284ea34230dec6ee
VasyaBilak <vasyabilak.vb@gmail.com>
2019-02-02 20:30:05 +0200

    working player

--- js/my.js
+++ js/my.js
@@ -7,26 +7,49 @@ class VideoPlayer {
       this.toggle = this.player.querySelector('.toggle');
       this.skipButtons = this.player.querySelectorAll('[data-skip]');
       this.ranges = this.player.querySelectorAll('.player__slider');
+      this.mouseDown = false;
    }
 
    init() {
-      this.events();
+      this.events(self);
    }
 
-   events() {
+   events(self) {
       // this.video.addEventListener('click', this.togglePlay.bind(this));
-      this.video.addEventListener('click', e => this.togglePlay());
+      this.video.addEventListener('click', e => this.togglePlay(e));
+      this.video.addEventListener('timeupdate', e => this.handleProgress(e));
       this.toggle.addEventListener('click', e => this.togglePlay());
+      this.ranges.forEach(range => range.addEventListener('change', e => this.handleRangeUpdate(e)));
+      this.ranges.forEach(range => range.addEventListener('mousemove', e => this.handleRangeUpdate(e)));
+      this.skipButtons.forEach(btn => btn.addEventListener('click', e => this.skip(e)));
+      this.progress.addEventListener('click', e => this.scrub(e));
+      this.progress.addEventListener('mousemove', e => this.mouseDown && this.scrub(e));
+      this.progress.addEventListener('mousedown', () => this.mouseDown = true);
+      this.progress.addEventListener('mouseup', () => this.mouseDown = false);
    }
 
-   togglePlay() {
+   togglePlay(e) {
       const method = this.video.paused ? 'play' : 'pause';
-      this.toggle.textContent = this.video.paused ? '1' : '2';
+      this.toggle.textContent = this.video.paused ? '| |' : '<';
       this.video[method]();
    }
 
-   skip() {
+   handleRangeUpdate(e) {
+      this.video[e.target.name] = e.target.value;
+   }
 
+   skip(e) {
+      this.video.currentTime += parseFloat(e.target.dataset.skip);
+   }
+
+   handleProgress(e) {
+      const percent = (this.video.currentTime / this.video.duration) * 100;
+      this.progressBar.style.flexBasis = `${percent}%`;
+      // console.log(percent);
+   }
+
+   scrub(e) {
+         this.video.currentTime = (e.offsetX / this.progress.offsetWidth) * this.video.duration;
    }
 }
 
